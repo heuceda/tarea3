ba04e0648ad7f634124a16538933cfd976cd4086
heuceda <neuceda13@gmail.com>
2023-08-23 00:04:15 -0600

    Correción de rutas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tarea3/pages/home/home_page.dart';
 import 'package:tarea3/pages/login/login_page.dart';
 
 void main() {
@@ -15,7 +16,9 @@ class MainApp extends StatelessWidget {
       title: "Tarea 3",
       theme: ThemeData(useMaterial3: true),
       initialRoute: "Login",
-      routes:{"Home":(context) => const LoginPage()}
+      routes:{"Home":(context) => const HomePage(),
+      "Login":(context) => const LoginPage(),
+      }
     );
   }
 }
